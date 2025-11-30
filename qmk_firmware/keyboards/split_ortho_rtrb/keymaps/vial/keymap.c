79fa743592ca5db8287049937228167dacfe856f
StaSzzzs <sz.sota@gmail.com>
2025-11-30 22:48:15 +0900

    Refactor keymap for split ortho keyboard

--- qmk_firmware/keyboards/split_ortho_rtrb/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/split_ortho_rtrb/keymaps/vial/keymap.c
@@ -1,7 +1,8 @@
 #include QMK_KEYBOARD_H
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {};
 
-#include QMK_KEYBOARD_H
-
+if(0)
+{
 // レイヤー名を定義します
 enum layer_names {
     _BASE = 0,    // Layer 0: 通常のQWERTY配列
@@ -11,74 +12,76 @@ enum layer_names {
 };
 
 // Keyball61の正確なレイアウトマクロ名 (例: LAYOUT_keyball61) に置き換えてください
-#define LAYOUT_SPLITORTHO(...) LAYOUT(__VA_ARGS__) 
+// #define LAYOUT_SPLITORTHO(...) LAYOUT(__VA_ARGS__) 
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* レイヤー 0: _BASE は省略 */
-[_BASE] = LAYOUT_SPLITORTHO(
-        // 左手側
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-        KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        KC_LCTRL,MO(_L3_NUM),KC_LGUI,KC_LALT,MO(_L1_NAV),KC_SPC,
-        
-        // 右手側
-                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-                KC_H,    KC_J,    KC_K,    TD(0),   TD(1),   KC_QUOT, // TD(0), TD(1) はVial GUIで設定
-        KC_BSLS,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_BSPC, KC_DEL,  MO(_L2_FUNC) // MO(2) はVial GUIで設定
-    ),
+[0] = LAYOUT_SPLITORTHO(
+    // 左手側
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+    KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+    KC_LCTL, MO(_NUM),KC_LGUI, KC_LALT, MO(_NAV), KC_SPC,
+
+    // 右手側
+             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+             KC_H,    KC_J,    KC_K,    TD(0),   TD(1),   KC_QUOT,
+    KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+    KC_BSPC, KC_DEL,  MO(_FUNC)
+),
 
     /* レイヤー 1: _L1_NAV (画像の内容をここに配置) */
-    [_L1_NAV] = LAYOUT_SPLITORTHO(
-        // 左手側: F1-F5, Navigation, Mouse Keys, Vol
-        _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,
-        _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,
-        _______, _______, _______, _______, KC_INSERT,KC_LEFT, KC_DOWN, KC_RGHT,
-        _______, _______, _______, _______, _______, _______, KC_LGUI, KC_MUTE, // LGUI(K), M2(Mute)
-        _______, _______, _______, _______, _______, KC_VOLU, _______, _______,
+    [1] = LAYOUT_SPLITORTHO(
+    // 左手側
+    _______, _______, _______, _______, KC_F1,   KC_F2,
+    _______, _______, _______, _______, KC_HOME, KC_UP,
+    _______, _______, _______, _______, KC_END,  KC_DOWN,
+    _______, _______, _______, _______, KC_LEFT, KC_RGHT,
+    _______, _______, _______, _______, _______, KC_VOLU,
 
-        // 右手側: F6-F11, Numpad
-        KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,
-        KC_PGUP, _______, KC_7,    KC_8,    KC_9,    KC_F12,  _______, _______,
-        KC_PGDN, _______, KC_4,    KC_5,    KC_6,    KC_PLUS, _______, _______,
-        _______, KC_NUMLCK,KC_1,    KC_2,    KC_3,    KC_SLSH, _______, _______,
-        _______, _______, KC_0,    KC_DOT,  KC_MINS, _______, _______, _______
-    ),
+    // 右手側
+             KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
+             KC_PGUP, KC_INS,  KC_7,    KC_8,    KC_9,    KC_F9,
+             KC_PGDN, KC_DEL,  KC_4,    KC_5,    KC_6,    KC_F10,
+    KC_LGUI, KC_MUTE,KC_1,    KC_2,    KC_3,    KC_F11,
+    _______, _______, KC_0,    
 
     /* レイヤー 2: _L2_FUNC (画像の内容をここに配置) */
-    [_L2_FUNC] = LAYOUT_SPLITORTHO(
-        // 左手側
-        _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,
-        _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,
-        _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT,
-        _______, _______, _______, _______, _______, _______, KC_TRNS, KC_MS_BTN3, // M3
-        _______, _______, _______, _______, _______, KC_MS_BTN1, _______, _______, // M1
+    [2] = LAYOUT_SPLITORTHO(
+    // 左手側
+    _______, _______, _______, _______, KC_F1,   KC_F2,
+    _______, _______, _______, _______, KC_HOME, KC_UP,
+    _______, _______, _______, _______, KC_END,  KC_DOWN,
+    _______, _______, _______, _______, KC_LEFT, KC_RGHT,
+    _______, _______, _______, _______, _______, KC_VOLU,
 
-        // 右手側
-        KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,
-        KC_MS_WH_UP, KC_MS_WH_DOWN, KC_MS_BTN3, KC_F12,  _______, _______, _______, _______, // Mouse Wheel Up/Down, Mouse 3
-        KC_MS_BTN4, KC_MS_BTN1, KC_MS_BTN2, _______, _______, _______, _______, _______, // Mouse 4, Mouse 1, Mouse 2
-        _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______
-    ),
+    // 右手側（マウス操作中心）
+             KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
+             KC_MS_WH_UP, KC_MS_WH_DOWN, KC_MS_BTN3, KC_F9, _______, KC_F10,
+             KC_MS_BTN4, KC_MS_BTN1, KC_MS_BTN2, _______, _______, KC_F11,
+    _______, _______, _______, _______, _______, KC_F12,
+    _______, _______, _______, _______, _______, _______
+),
 
     /* レイヤー 3: _L3_NUM (画像の内容をここに配置) */
-    [_L3_NUM] = LAYOUT_SPLITORTHO(
-        // 左手側
-        _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,
-        _______, _______, _______, _______, KC_LGUI, KC_TRNS, KC_LGUI, KC_TRNS, // LGui(T), (Up)
-        _______, _______, _______, _______, KC_TRNS, LCG(KC_LEFT), LCG(KC_DOWN), LCG(KC_RGHT), // (Left), (Down), (Right)
-        _______, _______, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, // Mouse Accel 0/1/2 labels are for Vial config
-        _______, _______, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, // M1 label is for Vial config
+    [3] = LAYOUT_SPLITORTHO(
+       // 左手側
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+    KC_LGUI, KC_TAB,  KC_UP,   KC_DOWN, KC_LEFT, KC_RGHT,
+    _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2,
+    _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,
 
-        // 右手側
-        KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,
-        KC_F12,  _______, LCG(KC_DOWN), LCG(KC_LEFT), LSG(KC_RGHT), _______, _______, _______, // LCG(D), LCG(Left), LSG(Right)
-        _______, _______, LCG(KC_LEFT), LCG(KC_RGHT), LSG(KC_RGHT), _______, _______, _______, // LCG(Left), LCG(Right), LSG(Right)
-        _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______
-    )
+    // 右手側
+             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+             _______, _______, _______, _______, _______, KC_LCTL,
+             _______, _______, _______, _______, _______, KC_LALT,
+    _______, _______, _______, _______, _______, KC_LSFT,
+    _______, _______, _______, _______, _______, KC_BTN1
+),
+};
+
 };
