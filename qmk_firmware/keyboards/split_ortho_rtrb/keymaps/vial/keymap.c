214ae77c88f213de339e32b0b7cbb68f16a72ba9
StaSzzzs <sz.sota@gmail.com>
2025-11-30 21:53:15 +0900

    Update keymap.c

--- qmk_firmware/keyboards/split_ortho_rtrb/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/split_ortho_rtrb/keymaps/vial/keymap.c
@@ -15,16 +15,20 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* レイヤー 0: _BASE は省略 */
-    [_BASE] = LAYOUT_SPLITORTHO(
-        // ここにレイヤー0のキーマップを定義 (前回の回答参照)
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+[_BASE] = LAYOUT_SPLITORTHO(
+        // 左手側
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+        KC_LCTRL,MO(_L3_NUM),KC_LGUI,KC_LALT,MO(_L1_NAV),KC_SPC,
+        
+        // 右手側
+                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+                KC_H,    KC_J,    KC_K,    TD(0),   TD(1),   KC_QUOT, // TD(0), TD(1) はVial GUIで設定
+        KC_BSLS,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+        KC_BSPC, KC_DEL,  MO(_L2_FUNC) // MO(2) はVial GUIで設定
     ),
 
     /* レイヤー 1: _L1_NAV (画像の内容をここに配置) */
